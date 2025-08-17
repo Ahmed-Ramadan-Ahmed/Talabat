d1850a2abcc6dba93e90f54d9333604706868646
Ahmed Ramadan <aramadan442000@gmail.com>
2025-08-17 13:48:55 +0300

    Get Types & Get Brands

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -12,10 +12,15 @@ namespace Talabat.APIs.Controllers
     public class ProductsController : APIBaseController
     {
         private readonly IGenericRepository<Product> _productRepo;
-        private readonly IMapper _mapper ;
-        public ProductsController(IGenericRepository<Product> ProductRepo, IMapper mapper)
+        private readonly IGenericRepository<ProductBrand> _brandRepo;
+        private readonly IGenericRepository<ProductType> _typeRepo;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IGenericRepository<Product> ProductRepo, IGenericRepository<ProductBrand> brandRepo, IGenericRepository<ProductType> typeRepo, IMapper mapper)
         {
             _productRepo = ProductRepo;
+            _brandRepo = brandRepo;
+            _typeRepo = typeRepo;
             _mapper = mapper;
         }
 
@@ -56,6 +61,35 @@ namespace Talabat.APIs.Controllers
             {
                 return Ok(ex.Message);
             }
+        }
+
+
+        [HttpGet("Types")]
+        [ProducesResponseType(typeof(IEnumerable<ProductType>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<ProductType>>> GetTypes()
+        {
+            var spec = new BaseSpecifications<ProductType>();
+            var types = await _typeRepo.GetAllAsync(spec);
+            if (types is null || !types.Any())
+            {
+                return NotFound(new ApiResponse(404));
+            }
+            return Ok(types);
+        }
+
+        [HttpGet("Brands")]
+        [ProducesResponseType(typeof(IEnumerable<ProductBrand>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<ProductBrand>>> GetBrands()
+        {
+            var spec = new BaseSpecifications<ProductBrand>();
+            var brands = await _brandRepo.GetAllAsync(spec);
+            if (brands is null || !brands.Any())
+            {
+                return NotFound(new ApiResponse(404));
+            }
+            return Ok(brands);
         }
     }
 }
