f3a237879c69b5931773d4ad63238944356bec69
Ahmed Ramadan <aramadan442000@gmail.com>
2025-08-24 08:06:43 +0300

    Register End Point

---
+++ Talabat.APIs/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.DTOs;
+using Talabat.APIs.Errors;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.APIs.Controllers
+{
+    
+    public class AccountsController : APIBaseController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        // register
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO)
+        {
+            var user = new AppUser()
+            {
+                DisplayName = registerDTO.DisplayName,
+                Email = registerDTO.Email,
+                UserName = registerDTO.Email.Split('@')[0],
+                PhoneNumber = registerDTO.PhoneNumber
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDTO.Password); 
+            if(!result.Succeeded)
+            {
+                return BadRequest(new ApiResponse(400));
+            }
+
+            return Ok(new UserDTO()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "This is a token"
+            });
+        }
+    }
+}
---
+++ Talabat.APIs/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Talabat.APIs.DTOs
+{
+    public class UserDTO
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
